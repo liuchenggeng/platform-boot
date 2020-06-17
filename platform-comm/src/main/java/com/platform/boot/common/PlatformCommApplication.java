cd771fc38d9b86c6a48f8ef2831c6c9715e8116b
liuchenggeng <liuchenggeng@126.com>
2020-06-18 05:46:54 +0800

    添加common模块

---
+++ platform-comm/src/main/java/com/platform/boot/common/PlatformCommApplication.java
@@ -0,0 +1,14 @@
+package com.platform.boot.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatformCommApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlatformCommApplication.class, args);
+    }
+
+
+}
